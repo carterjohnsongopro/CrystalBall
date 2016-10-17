a3e7c7b4f720dfa5fb9f03243be144a2f668ec64
Carter Johnson <carterjohnsongopro@gmail.com>
2016-10-16 18:03:05 -0700

    close

--- app/src/main/java/com/example/student/crystalball/MainActivity.java
+++ app/src/main/java/com/example/student/crystalball/MainActivity.java
@@ -51,10 +51,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        sensorManager.unregisterListener(sensorListener);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(sensorListener, accelerometer, sensorManager.SENSOR_DELAY_NORMAL)
     }
 }
