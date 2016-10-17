a29b536fa0843b27cc49d09e416cda4ddb34239a
Carter Johnson <carterjohnsongopro@gmail.com>
2016-10-16 17:18:00 -0700

    m5 now
    on m5

--- app/src/main/java/com/example/student/crystalball/MainActivity.java
+++ app/src/main/java/com/example/student/crystalball/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         answerText = (TextView) findViewById(R.id.answerText);
-        answerText.setText("reply is hazy try again tommorow");
+        answerText.setText(Predictions.get().getPrediction);
     }
 }
