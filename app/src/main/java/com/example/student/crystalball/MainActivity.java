21459b9334358490f1eb41503a069d959c0bec9d
Carter Johnson <carterjohnsongopro@gmail.com>
2016-10-16 17:29:09 -0700

    on m5

--- app/src/main/java/com/example/student/crystalball/MainActivity.java
+++ app/src/main/java/com/example/student/crystalball/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.student.crystalball;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,6 +11,24 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView answerText;
+
+    private SensorManager sensorManager;
+    private float acceleration;
+    private  float currentAcceleration;
+    private float previousAcceleration;
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
